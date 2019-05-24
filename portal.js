363dcef675f59c165bdfa57359e6e83892c668c5
Andi Andreas <nexuister@gmail.com>
2019-05-24 02:47:01 -0400

    Rewrite portal using REPL module

--- portal.js
+++ portal.js
@@ -1,15 +1,10 @@
 const WebSocket = require("ws");
 require("dotenv").config();
-const readline = require("readline");
+const replServer = require("repl");
 const ENDPOINT = process.env.ENDPOINT;
 const AUTH = process.env.PASSWORD;
 
 const ws = new WebSocket(ENDPOINT);
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 let conversations = [];
 let selectedConvo = null;
@@ -24,8 +19,48 @@ let send = obj => {
   );
 };
 
+let eval = msg => {
+  let shouldSend = true;
+  if (!selectedConvo) {
+    console.error("\nNo selected conversation!");
+    shouldSend = false;
+  }
+  if (msg.length < 2) {
+    console.error("\nMessage too short!");
+    shouldSend = false;
+  }
+  if (shouldSend) send({ action: "send", msg, uuidTo: selectedConvo.uuid });
+  repl.displayPrompt();
+};
+
+let repl = replServer.start({
+  prompt: ">> ",
+  eval
+});
+
+repl.defineCommand("c", () => send({ action: "hello" })); // trigger conversations list
+
+repl.defineCommand("s", i => {
+  selectedConvo = conversations[i];
+  repl.setPrompt(`${selectedConvo.nickname} >> `);
+  repl.displayPrompt();
+  send({ action: "list", for: selectedConvo.uuid });
+});
+
+repl.defineCommand("q", () => {
+  selectedConvo = null;
+  repl.setPrompt(">> ");
+  repl.displayPrompt();
+  send({ action: "hello" });
+});
+
+repl.on("exit", () => {
+  console.log("Goodbye.");
+  ws.close();
+});
+
 ws.on("open", () => {
-  console.log("Connected to live chat backend.");
+  console.log("\nConnected to live chat backend.");
   send({ action: "hello" });
   setInterval(() => send({ action: "ping " }), 30000); // ping every 30 seconds
 });
@@ -33,14 +68,7 @@ ws.on("open", () => {
 ws.on("message", data => {
   let json = JSON.parse(data);
   switch (json.type) {
-    case "reply":
-      let uuid = json.uuid;
-      let msg = json.msg;
-      let nickname = conversations.filter(convo => convo.uuid == uuid)[0].nickname;
-      console.log(`MSG FROM ${nickname}: ${msg}\n`);
-      break;
     case "andiItem":
-      console.log("CONVERSATIONS:");
       conversations = [];
       for (let uuid in json.result.conversations.M) {
         conversations.push({
@@ -50,58 +78,41 @@ ws.on("message", data => {
         });
       }
       let i = 0;
+      console.log("CONVERSATIONS:");
       for (convo of conversations) {
         console.log(`${i}. ${convo.nickname} ${convo.unread ? "(UNREAD)" : ""}`);
         i++;
       }
-      let pickConversation = () =>
-        rl.question("Pick a conversation: ", i => {
-          if (i >= 0 && i < conversations.length) {
-            selectedConvo = conversations[i];
-            console.log(`--- HISTORY WITH ${selectedConvo.nickname} ---`);
-            send({ action: "list", for: selectedConvo.uuid });
-          } else {
-            pickConversation();
-          }
-        });
-      pickConversation();
       break;
     case "history":
-      let connection = null;
+      console.log(`\nHISTORY WITH ${selectedConvo.nickname}:`);
       for (msgObj of json.result) {
         if (msgObj.type == "to") {
           console.log(`< ${msgObj.msg}`);
-          connection = msgObj.connection; // set connection to last connection used by sender
         } else {
           console.log(`>> ${msgObj.msg}`);
         }
       }
-      // it's not really a repl, but I didn't know what to call it
-      let repl = () =>
-        rl.question(">> ", msg => {
-          if (msg == "/q") {
-            selectedConvo = null;
-            return send({ action: "hello" }); // back to convos
-          } else {
-            if (msg.length < 1) return repl(); // don't send empty messages
-            send({ action: "send", msg, uuidTo: selectedConvo.uuid, connectionTo: connection });
-            repl();
-          }
-        });
-      repl();
+      break;
+    case "reply":
+      let uuid = json.uuid;
+      let msg = json.msg;
+      let nickname = conversations.filter(convo => convo.uuid == uuid)[0].nickname;
+      console.log(`\n< ${nickname}: ${msg}`);
       break;
     case "sendError":
-      console.log("X Delivery Error\n");
+      console.log("\nX Delivery Error");
       break;
   }
+  repl.displayPrompt();
 });
 
 ws.on("error", err => {
-  console.log(`X Connection error: ${err}.`);
+  console.error(`\nX Connection error: ${err}.`);
   process.exit(1);
 });
 
 ws.on("close", () => {
-  console.log("X Connection to live chat closed.");
+  console.error("\nX Connection to live chat closed.");
   process.exit(1);
 });
